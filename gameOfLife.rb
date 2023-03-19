fed729883e2e2d46e38aecab0c03420e2e826230
Mike <mwelponer@gmail.com>
2023-03-19 16:27:05 +0100

    in-place version of next function done

--- gameOfLife.rb
+++ gameOfLife.rb
@@ -14,10 +14,7 @@ def printGrid(grid, generation)
     end
 end
 
-def getNext(grid)
-    dirs = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1],
-      [-1, -1], [-1, 1], [1, -1]]
-
+def getNext(grid, dirs)
     rows, cols = grid.size, grid[0].size
     res = Array.new(rows) { Array.new(cols) { 0 } }
 
@@ -50,6 +47,64 @@ def getNext(grid)
     return res
 end
 
+def getNextInplace(grid, dirs)
+    rows, cols = grid.size, grid[0].size
+
+    for r in 0...rows do
+        for c in 0...cols do
+            # code: keeps track of the previous configuration
+            # first next  code
+            # 0     0     0
+            # 1     0     1
+            # 0     1     2
+            # 1     1     3
+            # count neighbors
+            neighbors = 0
+            for dr, dc in dirs
+                row, col = r + dr, c + dc
+                #puts row.to_s + ' ' + col.to_s
+                if row >= 0 and row < rows and col >= 0 and col < cols
+                    if grid[row][col] == 1 or grid[row][col] == 3
+                        neighbors += 1
+                    end
+                end
+            end
+            #puts 'neighbors: ' + neighbors.to_s
+
+            # evaluate next inplace
+            if grid[r][c] == 1 # cell is alive
+                if neighbors >= 2 and neighbors <= 3 # condition to remain alive
+                    grid[r][c] = 3 # from 1 to 1, code is 3
+                else
+                    grid[r][c] = 1 # from 1 to 0, code is 1
+                end
+            else # cell is dead
+                if neighbors == 3 # condition to get alive
+                    grid[r][c] = 2 # from 0 to 1, code is 2
+                # else condition to remain dead, from 0 to 0, code is already 0
+                end
+            end
+        end
+    end
+
+    # convert code to next
+    for r in 0...rows do
+        for c in 0...cols do
+            if grid[r][c] == 1
+                grid[r][c] = 0
+            elsif grid[r][c] != 0
+              grid[r][c] = 1
+            end
+        end
+    end
+
+    return grid
+end
+
+################################################################################
+dirs = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1],
+  [-1, -1], [-1, 1], [1, -1]]
+
 # puts 'Hello, world!'
 # a = Array.new(3) { Array.new(2) { 0 } }
 grid = [[0,0,0,0,1,0,0,0],
@@ -58,5 +113,6 @@ grid = [[0,0,0,0,1,0,0,0],
         [0,0,0,0,0,0,0,0]]
 
 printGrid(grid, 0)
-grid = getNext(grid)
+#grid = getNext(grid, dirs)
+grid = getNextInplace(grid, dirs)
 printGrid(grid, 1)
